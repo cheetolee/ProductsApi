9c78597332711ee68f75e7524d513bee22cbe230
cheetolee <108768815+cheetolee@users.noreply.github.com>
2022-10-12 16:54:37 -0400

    5th commit

--- src/main/java/ca/vanier/Main.java
+++ src/main/java/ca/vanier/Main.java
@@ -1,20 +1,21 @@
 package ca.vanier;
- 
+
 import java.util.*;
- 
+
 public class Main {
     public static void main(String[] args) {
         int flag = 0;
-        int i;
         ArrayList<CarPojo> cars = new ArrayList<>();
- 
-        for (i = 0; flag == 0; i++) {
+
+        for (int i = 0; flag == 0; i++) {
             System.out.println("Enter car model,brand,year");
-            cars.add(new CarPojo(args[i], args[i++], Integer.parseInt(args[i + 2])));
+            cars.add(new CarPojo(args[i], args[i + 1], Integer.parseInt(args[i + 2])));
             System.out.println("do you want to add car if yes press 0 if no press 1");
-            flag = Integer.parseInt(args[i + 3]);
+            flag = Integer.parseInt(args[i + 1]);
         }
+        for (CarPojo car : cars)
+            System.out.println(car);
         System.out.println("Saving to database....");
- 
+
     }
 }
